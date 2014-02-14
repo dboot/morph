fbc0ad38b6242426f155d32d4c137a6308b83fde
Pebody <duruku@gmail.com>
2014-02-14 03:56:30 +0200

    ww

--- MorphMapper.py
+++ MorphMapper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.3
 
 import sys
 import re
@@ -14,12 +14,9 @@ if __name__ == "__main__":
     T = morph.Tagger()
 
     for line in sys.stdin:
-        url, raw = line.split('\t', 1)
+        key, value = line.split('\t', 1)
 
-        if (re.search('\?\s*$', raw)):
-            continue
-
-        normalized = bank_names.replace_names(raw.lower())
+        normalized = bank_names.replace_names(value.lower())
 
         words = normalized[0].split(' ')
 
@@ -31,15 +28,20 @@ if __name__ == "__main__":
             if (word and word not in ["-", ":", "%", "$"]):
                 sentence.append((word, tuple()))
 
+        if (normalized[1].__len__() == 0):
+            continue
+
+        if (re.search('\?\s*$', value)):
+            continue
+
         if (sentence.__len__() > 14):
             continue
 
         labeled = T.label(sentence)
 
-        s = etree.Element('S', URL=url, RAW=re.sub('\n', '', raw))
+        s = etree.Element('S', RAW=re.sub('\n', '', value))
 
         i = 1
-        has_obj = False
         for word in labeled:
 
             if word[2]:
@@ -54,15 +56,12 @@ if __name__ == "__main__":
 
             w = etree.SubElement(s, 'W', FEAT=tags, ID=str(i))
             if word[0].istitle():
-                w.text = normalized[1].pop(0)
-                w.set('OBJECT', 'true')
-                has_obj = True
+                w.text = normalized[1][0][0]
+                w.set('OBJECT', normalized[1][0][1])
+                normalized[1].pop(0)
             else:
                 w.text = word[0]
 
-            i+=1
+            i += 1
 
-        if has_obj:
-            print(url + '\t' + etree.tostring(s, encoding='unicode'))
-
-        
+        print(key + '\t' + etree.tostring(s, encoding='unicode'))
--- bank_names.py
+++ bank_names.py
@@ -16,30 +16,36 @@ bank_form = {'S.m.nom.sg': 'Банк',
              'S.m.ins.pl': 'Банками',
              'S.m.prep.pl': 'Банках', }
 
+nom_pade = 'S.m.nom.sg'
+
 
 def make_dict(filename='banks.txt'):
     """ Create a dict { bank name : type of word form }"""
 
     NAMES = {}
+    p = 'Nom.sg'
+
     with open(filename, 'r') as bank_names:
         for line in bank_names:
             line = line.strip()
             if line is not '':
                 i = line.find('S.m.')
-                NAMES[line[:i - 1].title()] = line[i:]
+                if nom_pade in line:
+                    p = line[:i - 1].title()
+                NAMES[line[:i - 1].title()] = (line[i:], p)
     return NAMES
 
 
 def replace_names(post):
     """ Replace all matches with known banks' names.
-        Return a tuple (edited post, List of found and replaced bank names).
+        Return a tuple (Edited Post, List of tuples: (found and replaced Bank Name, Nom.sg form of bank name)).
         List has exactly the same order """
 
     dictio = make_dict()
-    s = "(" + "|".join(sorted(list(dictio.keys()), key=len, reverse=True)) + ")\\b"
+    s = r'\b(' + "|".join(sorted(list(dictio.keys()), key=len, reverse=True)) + r')\b'
     pattern = re.compile(s, re.I)
     found_names = pattern.findall(post)
 
     for match in found_names:
-        post = re.compile(match).sub(bank_form[dictio[match.title()]], post)
-    return post, [name.title() for name in found_names]
+        post = re.compile(r'\b' + match + r'\b').sub(bank_form[dictio[match.title()][0]], post)
+    return post, [(name.title(), dictio[name.title()][1] ) for name in found_names]
